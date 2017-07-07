e142f5f9c409a6ea30c40a6467f7f0ec7ab6b9c8
miiiii1 <s1542063@s.chibakoudai.jp>
2017-07-07 13:30:51 +0900

    1

--- sample/index.php
+++ sample/index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>ろくまる農園</title>
+        <title>スズキヤ</title>
     </head>
     <body>
         <?php
