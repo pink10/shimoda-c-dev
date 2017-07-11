e86da9f6990bcc8821e20995e00d0d5af6f98757
shimoda4 <shimoda4@users.noreply.github.com>
2017-07-12 04:16:57 +0900

    shimoda-ok

--- sample/staff_login/staff_login_check.php
+++ sample/staff_login/staff_login_check.php
@@ -51,6 +51,7 @@ else
 	$_SESSION['login']=1;
 	$_SESSION['staff_code']=$staff_code;
 	$_SESSION['staff_name']=$rec['name'];
+        ob_end_clean();
 	header('Location:staff_top.php');
 	exit();
 }
